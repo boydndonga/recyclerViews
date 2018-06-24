6fac9a9b558f2965cba2174201b7688b955bef3e
Boyd Ndonga <boydbugle@gmail.com>
2018-06-24 15:28:51 +0300

    declare recycler view in main activity

--- app/src/main/java/com/example/ndonga/recyclerviews/MainActivity.java
+++ app/src/main/java/com/example/ndonga/recyclerviews/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.ndonga.recyclerviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
+    private MyAdapter mMyAdapter;
+    private List<ListItem> mListItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mListItem = new ArrayList<>();
     }
 }
--- app/src/main/java/com/example/ndonga/recyclerviews/MyAdapter.java
+++ app/src/main/java/com/example/ndonga/recyclerviews/MyAdapter.java
@@ -1,31 +1,59 @@
 package com.example.ndonga.recyclerviews;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
 
+
+    private List<ListItem> mListItem;
+    private Context mContext;
+
+    public MyAdapter(List<ListItem> mListItem, Context mContext) {
+        this.mListItem = mListItem;
+        this.mContext = mContext;
+    }
+
+    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_layout,parent,false);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        ListItem listItem = mListItem.get(position);
+        holder.mHeadingTextView.setText(listItem.getHead());
+        holder.mDescriptionTextView.setText(listItem.getDesc());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mListItem.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        
+        @BindView(R.id.headingTextView)
+        TextView mHeadingTextView;
+        @BindView(R.id.descriptionTextView)
+        TextView mDescriptionTextView;
+
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
+
         }
     }
 }
